8254ecb321bebaf1f14428745d4a7f6942ecb7d0
Tysuiku <timothydong41@gmail.com>
2023-03-02 18:32:38 -0500

    finish clock and start myBind

--- callBackExcercises.js
+++ callBackExcercises.js
@@ -16,12 +16,57 @@ class Clock {
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
+    const timeString = [this.hours, this.minutes, this.seconds].join(":");
+    console.log(timeString);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    this._incrementSeconds();
+    this.printTime();
+  }
+
+  _incrementSeconds() {
+    // 1. Increment the time by one second.
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this._incrementMinutes();
+    }
+  }
+
+  _incrementMinutes() {
+    this.minutes += 1;
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this._incrementHours();
+    }
+  }
+
+  _incrementHours() {
+    this.hours = (this.hours + 1) % 24;
   }
 }
 
 const clock = new Clock();
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function () {
+  console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
