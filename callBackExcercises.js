0864a3129f3aeb0ecad60b728b5454da2621c2d4
Tysuiku <timothydong41@gmail.com>
2023-03-02 15:39:51 -0500

    start working on project after setting up environment

---
+++ callBackExcercises.js
@@ -0,0 +1,27 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.
+    const currentTime = new Date();
+    this.hour = currentTime.getHours();
+    this.minutes = currentTime.getMinutes();
+    this.seconds = currentTime.getSeconds();
+
+    this.printTime();
+    setInterval(this._tick.bind(this), 1000);
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+  }
+}
+
+const clock = new Clock();
